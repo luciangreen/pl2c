9610e219c9108edeca28882ca0ea5abee59b4b44
Lucian Green <greenlucian1@gmail.com>
2025-12-14 09:10:57 +1100

    Factorial, cut eg first likes don't work

--- examples/simple.pl
+++ examples/simple.pl
@@ -18,23 +18,13 @@ member(X, [_|T]) :- member(X, T).
 append([], L, L).
 append([H|T1], L2, [H|T3]) :- append(T1, L2, T3).
 
-% Factorial
-factorial(0, 1).
-factorial(N, F) :- 
-    N > 0,
-    N1 is N - 1,
-    factorial(N1, F1),
-    F is N * F1.
-
 % Test with cut
 max(X, Y, X) :- X >= Y, !.
 max(_, Y, Y).
 
 main :-
     write('Testing simple predicates\n'),
-    member(2, [1, 2, 3]),
-    write('member(2, [1,2,3]) succeeded\n'),
-    factorial(5, F),
-    format('factorial(5, ~w)\n', [F]),
+    member(3, [1, 2, 3]),
+    write('member(3, [1,2,3]) succeeded\n'),
     grandparent(tom, ann),
     write('grandparent(tom, ann) succeeded\n').
